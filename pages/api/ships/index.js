3052bc526138ae43c517d27f76f99d1647167345
RedKamo <camiloandressuarez1@gmail.com>
2021-03-01 19:42:54 -0500

    adding length to api/ships

--- pages/api/ships/index.js
+++ pages/api/ships/index.js
@@ -4,10 +4,11 @@ import DB from '../../../database/db'
 const allShips= async (request, Response) => {
   const db = new DB()
   const allEntries = await db.getAll()
+  const length = allEntries.length
 
   Response.statusCode= 200 // ok
   Response.setHeader('Content-type', 'application/json')
-  Response.end(JSON.stringify({ data : allEntries}))
+  Response.end(JSON.stringify({ data : allEntries, length}))
 }
 
 export default allShips
