3eb5966728dbbb825395eadfb7bc738180f9b1bf
RedKamo <camiloandressuarez1@gmail.com>
2021-03-02 19:02:58 -0500

    some changes

--- pages/api/ships/[id].js
+++ pages/api/ships/[id].js
@@ -1,12 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+//import { NextApiRequest, NextApiResponse } from 'next'
 import DB from '../../../database/db'
 //import shipItem from '../../product/[id]'
 
 
- const shipDetail= async (req, res) => {
+ const shipDetail= async ( request, response) => {
   const db = new DB()
 
-  const id = req.query.id
+  const id = request.query.id.toString()
   const ship = await db.getById(id)
 
   // way 1 
@@ -17,7 +17,7 @@ import DB from '../../../database/db'
   //TODO: there is an easy way to do the same of the 3 lines above in 1 line:  
  
   //way 2 
-  res.status(200).json(ship)
+  response.status(200).json(ship)
 } 
 
 
