f53dd8f703bfd4209dc6b125ee56635b9780f703
RedKamo <camiloandressuarez1@gmail.com>
2021-03-09 15:26:50 -0500

    add footer styles and social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,19 @@
-import React from "react";
+import { AiFillGithub, AiFillTwitterCircle } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h3>© 2021 This site is made by Camilo Suarez 👨🏾‍🚀.</h3>
+      <h3>
+        © 2021 This site is made by <span>Red</span>Kamo👨🏾‍🚀.
+      </h3>
+      <div className="footer__social">
+        <a href="https://github.com/RedKamo" target="_blank">
+          <AiFillGithub />
+        </a>
+        <a href="https://twitter.com/RedKamo_" target="_blank">
+          <AiFillTwitterCircle />
+        </a>
+      </div>
     </div>
   );
 };
